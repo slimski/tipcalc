620d91ef18f2bccb0bc621a630914cec3de64c7a
Igor Nabokov <inabokov@65apps.com>
2017-05-10 11:21:36 +0400

    remove some trash

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,100 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var numberToCalc: Int = 0
-    
     @IBOutlet weak var label: UILabel!
 
-    @IBOutlet weak var one: UIButton!
-
-    @IBOutlet weak var two: UIButton!
-    
-    @IBOutlet weak var three: UIButton!
-    
-    @IBOutlet weak var four: UIButton!
-    
-    @IBOutlet weak var five: UIButton!
-    
-    @IBOutlet weak var six: UIButton!
-    
-    @IBOutlet weak var seven: UIButton!
-    
-    @IBOutlet weak var eight: UIButton!
-    
-    @IBOutlet weak var nine: UIButton!
-    
-    @IBOutlet weak var dotButton: UIButton!
-    
-    @IBOutlet weak var zero: UIButton!
-    
-    @IBOutlet weak var delButton: UIButton!
-    
-    @IBOutlet weak var calcButton: UIButton!
-    
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         label.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func appendOne(_ sender: UIButton) {
-        let buttonNumber = 1
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
     
-    @IBAction func appendTwo(_ sender: UIButton) {
-        let buttonNumber = 2
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendThree(_ sender: UIButton) {
-        let buttonNumber = 3
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendFour(_ sender: UIButton) {
-        let buttonNumber = 4
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendFive(_ sender: UIButton) {
-        let buttonNumber = 5
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendSix(_ sender: UIButton) {
-        let buttonNumber = 6
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendSeven(_ sender: UIButton) {
-        let buttonNumber = 7
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendEight(_ sender: UIButton) {
-        let buttonNumber = 8
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
-    }
-    
-    @IBAction func appendNine(_ sender: UIButton) {
-        let buttonNumber = 9
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
+    @IBAction func appendDigit(_ sender: UIButton) {
+        if let digit = sender.titleLabel?.text {
+            label.text?.append(digit)
+            print(label.text ?? "label hasn't text")
+        } else {
+            print("button hasn't title")
+        }
     }
     
     @IBAction func appendDot(_ sender: UIButton) {
@@ -113,40 +35,38 @@ class ViewController: UIViewController {
             label.text?.append("0.")
         } else {
         label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
         }
-    }
-    
-    @IBAction func appendZero(_ sender: UIButton) {
-        let buttonNumber = 0
-        label.text?.append("\(buttonNumber)")
-        print(numberToCalc)
     }
     
     @IBAction func deleteStuff(_ sender: UIButton) {
         if (label.text?.characters.count)! > 0 {
             print("More than zero! Delete")
-            label.text? = String(self.label.text!.characters.dropLast())
+            label.text = String(self.label.text!.characters.dropLast())
         } else {
-            label.text? = ""
+            label.text = ""
         }
         
     }
     
     @IBAction func calcStuff(_ sender: UIButton) {
-        print("\(label.text!)")
-        if var converted = Double(label.text!) {
-        converted *= 1.20
-        print("After 20% tips it's going to be" + "\(converted)")
-        converted = converted.rounded(.up)
-        print("After rounding it's" + "\(converted)")
-        if converted > 20.00 {
-        while converted.truncatingRemainder(dividingBy: 5.00) > 0 {
-            converted += 1.00
-            print("\(converted)")
-            }
+        guard let text = label.text else {
+            print("label hasn't text")
+            return;
         }
-        label.text? = ("\(converted)")
+        
+        print(text)
+        if var converted = Double(text) {
+            converted *= 1.20
+            print("After 20% tips it's going to be" + "\(converted)")
+            converted = converted.rounded(.up)
+            print("After rounding it's" + "\(converted)")
+            if converted > 20.00 {
+                while converted.truncatingRemainder(dividingBy: 5.00) > 0 {
+                    converted += 1.00
+                    print("\(converted)")
+                }
+            }
+            label.text = ("\(converted)")
         }
     }
 }
